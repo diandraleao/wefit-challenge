3fbba01d1d1ba562db72f1b058adb3d859ea9284
Diandra <leao.diandra@gmail.com>
2024-02-18 15:15:17 -0300

    init

---
+++ src/app/carrinho/page.tsx
@@ -0,0 +1,15 @@
+'use client'
+import Image from "next/image";
+import styles from "./page.module.css";
+import { useState, useEffect } from "react";
+import GetInitialProps from 'next'
+
+
+
+export default function Carrinho() {
+  
+  return (
+    <h1>Carrinho</h1>
+  );
+}
+
